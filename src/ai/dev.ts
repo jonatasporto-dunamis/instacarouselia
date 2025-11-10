d184a67a009fd139d9e5aa9230abe5dd180dcbc8
Jonatas Porto dos Santos <contato.jonatasporto@gmail.com>
2025-11-10 18:32:22 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ge

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,5 @@
+'use server';
 import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/generate-carousel-slides.ts';
-import '@/ai/flows/summarize-slide-content.ts';
-import '@/ai/flows/improve-slide-content-with-ai.ts';
-import '@/ai/flows/generate-ai-carousel-slides.ts';
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,23 @@
 
-import {genkit} from 'genkit';
+import {genkit, GenkitPlugin} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 import {config} from 'dotenv';
 
 config({path: '.env'});
 
+const plugins: GenkitPlugin[] = [];
+
+if (process.env.GEMINI_API_KEY) {
+  plugins.push(googleAI({apiKey: process.env.GEMINI_API_KEY}));
+  console.log('Using Gemini model.');
+}
+
+
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-2.5-flash',
+  plugins,
+  // This will be undefined if no key is provided, Genkit will throw an error.
+  // This is the desired behavior to alert the user.
+  // We recommend using a logger in production.
+  // logSinks: [new GoogleCloudLogger()],
+  // OpenTelemetry is instrumented automatically in App Hosting.
 });
