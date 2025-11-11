85632e048993b14791649ce807b98b8df4731c46
Jonatas Porto dos Santos <contato.jonatasporto@gmail.com>
2025-11-11 14:27:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8443257021-22a82",
+  "appId": "1:684777726641:web:7e3f139254913246d2b890",
+  "apiKey": "AIzaSyAxw4fLMm0Ue7-3PyzFPtmTK5yhvTxtQpc",
+  "authDomain": "studio-8443257021-22a82.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "684777726641"
+};
